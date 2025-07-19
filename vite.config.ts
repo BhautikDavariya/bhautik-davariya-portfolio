17d031d528a6b0e93d4b75a1149cee4409afb617
Bhautik Davariya <bhautikmerndevelopers@gmail.com>
2025-07-19 13:43:54 +0530

    minor change

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { createServer } from "./server";
 
+// changes
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   base: '/bhautik-davariya-portfolio/',
