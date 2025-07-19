5a99477a96d48c0ddee5947fa6ee566fe0daaf60
Bhautik Davariya <bhautikmerndevelopers@gmail.com>
2025-07-19 18:59:02 +0530

    remove server

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,14 @@
 import { defineConfig, Plugin } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { createServer } from "./server";
 
-// changes
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   base: '/bhautik-davariya-portfolio/',
-  server: {
-    host: "::",
-    port: 8080,
-  },
   build: {
-    outDir: "dist/spa",
+    outDir: "dist",
   },
-  plugins: [react(), expressPlugin()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./client"),
@@ -23,15 +17,43 @@ export default defineConfig(({ mode }) => ({
   },
 }));
 
-function expressPlugin(): Plugin {
-  return {
-    name: "express-plugin",
-    apply: "serve", // Only apply during development (serve mode)
-    configureServer(server) {
-      const app = createServer();
 
-      // Add Express app as middleware to Vite dev server
-      server.middlewares.use(app);
-    },
-  };
-}
+
+// import { defineConfig, Plugin } from "vite";
+// import react from "@vitejs/plugin-react-swc";
+// import path from "path";
+// // import { createServer } from "./server";
+
+// // changes
+// // https://vitejs.dev/config/
+// export default defineConfig(({ mode }) => ({
+//   // base: '/bhautik-davariya-portfolio/',
+//   // server: {
+//   //   host: "::",
+//   //   port: 8080,
+//   // },
+//   build: {
+//     outDir: "dist",
+//   },
+//   // plugins: [react(), expressPlugin()],
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./client"),
+//       "@shared": path.resolve(__dirname, "./shared"),
+//     },
+//   },
+// }));
+
+// // function expressPlugin(): Plugin {
+// //   return {
+// //     name: "express-plugin",
+// //     apply: "serve", // Only apply during development (serve mode)
+// //     configureServer(server) {
+// //       const app = createServer();
+
+// //       // Add Express app as middleware to Vite dev server
+// //       server.middlewares.use(app);
+// //     },
+// //   };
+// // }
